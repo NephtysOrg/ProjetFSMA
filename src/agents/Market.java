400d9b1a221099dfdb75de811f7db12ca0fe856e
rbary <bary.roland@gmail.com>
2016-01-12 23:47:09 +0100

    2nd commit (git user config on debian)

---
+++ src/agents/Market.java
@@ -0,0 +1,14 @@
+package agents;
+
+import java.util.List;
+
+import jade.core.AID;
+import jade.core.Agent;
+
+public class Market extends Agent {
+	
+	/** registered auctions **/
+	private Map<String, Auction> Auctions = new HashMap<String, Auction>();
+	
+
+}
